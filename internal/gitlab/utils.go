ce2081a64868229ddb3a8e070ba59e6d8cc1b18e
Malek Hamza <malek.hamza@gmx.de>
2024-06-14 23:58:37 +0200

    Implement util method for extracting ticket numbers

---
+++ internal/gitlab/utils.go
@@ -0,0 +1,17 @@
+package gitlab
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func ResolveJiraTicketFromTitle(title string, ticketNumber *string) error {
+	pattern := regexp.MustCompile(`([A-Z]+-\d+)`)
+	matches := pattern.FindStringSubmatch(title)
+	if len(matches) > 1 {
+		*ticketNumber = matches[0]
+		return nil
+	} else {
+		return fmt.Errorf("no Jira ticket found in the title '%s'", title)
+	}
+}
---
+++ internal/gitlab/utils_test.go
@@ -0,0 +1,34 @@
+package gitlab_test
+
+import (
+	"testing"
+
+	"mlhmz.xyz/go-gitlab-jira-dispatcher/internal/gitlab"
+)
+
+var goodTitles = map[string]string{
+	"PROJ-123":   "PROJ-123: Fixing bugs",
+	"PROJE-1234": "PROJE-1234: Fixing bugs",
+	"PROJ-1234":  "Merge reqeust for PROJ-1234",
+	"NICE-42":    "NICE-42: Fix connection timeout",
+	"NICE-425":   "NICE-425: Fix connection timeout with a really long title TEST-123",
+}
+
+func TestResolveJiraTicketFromTitle_FillsTicketNumber_OnTitleContainingTicketNumber(t *testing.T) {
+	for expected, title := range goodTitles {
+		ticketNumber := ""
+		if err := gitlab.ResolveJiraTicketFromTitle(title, &ticketNumber); err != nil {
+			t.Errorf("ResolveJiraTicketFromTitle failed: %v", err)
+		}
+		if ticketNumber != expected {
+			t.Errorf("ResolveJiraTicketFromTitle failed: expected %s, got %s", expected, ticketNumber)
+		}
+	}
+}
+
+func TestResolveJiraTicketFromTitle_ReturnsError_OnTitleNotContainingTicketNumber(t *testing.T) {
+	ticketNumber := ""
+	if err := gitlab.ResolveJiraTicketFromTitle("Update README.md", &ticketNumber); err == nil {
+		t.Error("ResolveJiraTicketFromTitle failed: expected an error, got nil")
+	}
+}
