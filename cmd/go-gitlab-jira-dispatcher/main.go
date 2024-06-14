c220f660c532dc28cf9e42654b03f574a237d44d
Malek Hamza <malek.hamza@gmx.de>
2024-06-14 23:22:35 +0200

    Implement initial working webhook logging route

--- cmd/go-gitlab-jira-dispatcher/main.go
+++ cmd/go-gitlab-jira-dispatcher/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"mlhmz.xyz/go-gitlab-jira-dispatcher/internal/gitlab"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+	app.Post("/webhook", func(c *fiber.Ctx) error {
+		var event *gitlab.MergeRequestEvent
+
+		log.Info(string(c.Body()[:]))
+
+		if err := c.BodyParser(&event); err != nil {
+			log.Error("Failed to parse webhook event", "error", err)
+			return c.SendStatus(400)
+		}
+
+		log.Info("Received webhook event with the action ", event.ObjectAttributes.Action, " and the name '", event.ObjectAttributes.Title, "'")
+
+		return c.SendStatus(200)
 	})
 
 	app.Listen(":3000")
