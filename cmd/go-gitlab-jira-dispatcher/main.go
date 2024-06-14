0629f08065453bd17aabb8f28b188a73328436da
Malek Hamza <malek.hamza@gmx.de>
2024-06-14 21:11:35 +0200

    Configure fiber and air

--- cmd/go-gitlab-jira-dispatcher/main.go
+++ cmd/go-gitlab-jira-dispatcher/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
 }
