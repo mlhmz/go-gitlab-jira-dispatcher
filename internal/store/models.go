dc6cbe53d3ab6fbd338235424671dc64beb96a37
Malek Hamza <malek.hamza@gmx.de>
2024-06-16 23:42:28 +0200

    Add title and projects field

--- internal/store/models.go
+++ internal/store/models.go
@@ -8,11 +8,13 @@ import (
 type WebhookConfig struct {
 	gorm.Model      `json:"-"`
 	ID              string `json:"id"`
+	Title           string `json:"title"`
 	ReadyForReview  int    `json:"ready_for_review"`
 	InReview        int    `json:"in_review"`
 	ReviewOK        int    `json:"review_ok"`
 	ReviewNotOK     int    `json:"review_not_ok"`
 	DevelopmentDone int    `json:"development_done"`
+	Projects        string `json:"projects"`
 }
 
 func (config *WebhookConfig) BeforeCreate(tx *gorm.DB) error {
